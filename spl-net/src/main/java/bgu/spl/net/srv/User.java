68be542137fdb18d5b2fac54d9409b4c5141a8d7
Davehai <73841682+Davehai@users.noreply.github.com>
2021-01-02 17:37:21 +0200

    build a class for user

---
+++ spl-net/src/main/java/bgu/spl/net/srv/User.java
@@ -0,0 +1,24 @@
+package bgu.spl.net.srv;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    String UserName;
+    int password;
+
+
+    public User(String Name, int Pass) {
+        UserName = Name;
+        password = Pass;
+
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public boolean PasswardCheck(int Pass) {
+        return password == Pass;
+    }
+}

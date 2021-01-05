6731c4b3654432454554316dc8e2a5e5afd7de7c
BatelBB <batelx13@gmail.com>
2021-01-05 20:35:40 +0200

    Chagned the getLine to fit

--- Client/include/connectionHandler.h
+++ Client/include/connectionHandler.h
@@ -31,7 +31,7 @@ public:
 	
     // Read an ascii line from the server
     // Returns false in case connection closed before a newline can be read.
-    bool getLine(std::string& line);
+    bool getLine(std::string& line, bool& terminate);
 	
 	// Send an ascii line from the server
     // Returns false in case connection closed before all the data is sent.
